93860d122f2fdaa36d0c90f8e40ab0b92d2ec744
Alvin Wei <yiwei1119.xavier@gmail.com>
2023-05-16 10:44:31 +0800

    tutorial map (done w/ minimao), main map (undone)

---
+++ Assets/Map/MiniMap/Show Map.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMap : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        foreach (Transform tran in collision.gameObject.transform.parent)
+        {
+            if ((tran.position - collision.transform.position).magnitude < 8)
+                tran.gameObject.layer = 10;
+        }
+    }
+
+}
