87af4f7264eed948ecb75e2e7c41dd175ba1e553
Calmtea <willy5209593@gmail.com>
2023-06-10 12:28:42 +0800

    Modify dirt and dark's magic sound effect. Add the ability to capture screenshots of specific camera views.

--- Assets/Scripts/ScreenshotManager.cs
+++ Assets/Scripts/ScreenshotManager.cs
@@ -3,30 +3,36 @@ using System.IO;
 
 public class ScreenshotManager : MonoBehaviour
 {
-
+    public Camera targetCamera;
     public string filenamePrefix = "Screenshot";
-
-
     public string savePath = "/Screenshots/";
-
-
     public KeyCode screenshotKey = KeyCode.T;
-
 
     void Update()
     {
-
         if (Input.GetKeyDown(screenshotKey))
         {
-            
             string filename = filenamePrefix + "_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
-
-            
             string filePath = Application.dataPath + savePath + filename;
 
-            
-            ScreenCapture.CaptureScreenshot(filePath);
+            // 创建一个临时的 RenderTexture 来存储目标相机的图像
+            RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+            targetCamera.targetTexture = renderTexture;
+            targetCamera.Render();
 
+            // 保存 RenderTexture 的图像到文件
+            RenderTexture.active = renderTexture;
+            Texture2D screenshotTexture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+            screenshotTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            screenshotTexture.Apply();
+            byte[] screenshotBytes = screenshotTexture.EncodeToPNG();
+            File.WriteAllBytes(filePath, screenshotBytes);
+
+            // 清理资源
+            Destroy(screenshotTexture);
+            RenderTexture.active = null;
+            targetCamera.targetTexture = null;
+            RenderTexture.Destroy(renderTexture);
 
             Debug.Log("Screenshot saved to " + filePath);
         }
